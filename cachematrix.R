fa06e0d1121fcfc5de1e8a28daf7be3635891d32
Martin Hieronymus <martin.hieronymus@nordakademie.de>
2014-07-26 20:15:25 +0200

    Revert "Solution"
    This reverts commit e71bea63ebf625b45f40a63b766ef1cb1c118283.

--- cachematrix.R
+++ cachematrix.R
@@ -1,49 +1,15 @@
+## Put comments here that give an overall description of what your
+## functions do
 
-############### ProgrammingAssignment2 ##########################
-## The following functions may be used, to compute the inverse ##
-## of a matrix and store it as cached data for further use.    ##
-## @author MHiero
-## @version 1.3
-
-############## makeCacheMatrix ##################################
-## This function creates a special "matrix" object that can    ##
-## cache its inverse.                                          ##
-## @param: Call parameter x, an invertible matrix              ##
-## @return:  cacheable matrix object                           ##
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setinv <- function(solve) m <<- solve
-  getinv <- function() m
-  list(set = set, get = get,
-       setinv = setinv,
-       getinv = getinv)
-  
+head(x)
 }
 
-############## cacheSolve ##################################
-## This function computes the inverse of the special      ##
-## "matrix" returned by makeCacheMatrix above. If the     ##
-## inverse has already been calculated (and the matrix    ##
-## has not changed), then cacheSolve should retrieve the  ##
-## inverse from the cache.                                ##
-##                                                        ##
-## @param: Call parameter x, a (Cache-)Matrix             ##
-## @return:  Inverse matrix                               ##
+
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  m <- x$getinv()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinv(m)
-  m
+        ## Return a matrix that is the inverse of 'x'
 }
